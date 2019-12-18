66a9ed74a4c5beac271c45a8e03e8e5e571535df
Andrea <andreaeva.lengyel@gmail.com>
2019-12-18 06:07:44 +0200

    Initial commit

---
+++ src/test/java/org/fasttrackit/pages/HomePage.java
@@ -0,0 +1,42 @@
+package org.fasttrackit.pages;
+
+import net.serenitybdd.core.annotations.findby.FindBy;
+import net.serenitybdd.core.pages.PageObject;
+import net.serenitybdd.core.pages.WebElementFacade;
+import net.thucydides.core.annotations.DefaultUrl;
+import org.openqa.selenium.WebDriver;
+
+@DefaultUrl("http://testfasttrackit.info/selenium-test/")
+public class HomePage extends PageObject {
+
+
+    @FindBy(css = "a[href*='/customer/account'] .label")
+    private WebElementFacade accountLink;
+
+    @FindBy(css = "a[title = 'Log In']")
+    private WebElementFacade loginLink;
+
+    @FindBy(id = "search")
+    private WebElementFacade searchField;
+
+    @FindBy(className = "search-button")
+    private WebElementFacade searchButton;
+
+    public void clickAccountLink(){
+        clickOn(accountLink);
+    }
+
+    public void clickOnLoginLink(){
+        clickOn(loginLink);
+    }
+
+    public void setSearchField(String text){
+        typeInto(searchField,text);
+    }
+
+    public void clickSearchButton(){
+        clickOn(searchButton);
+    }
+
+
+}
